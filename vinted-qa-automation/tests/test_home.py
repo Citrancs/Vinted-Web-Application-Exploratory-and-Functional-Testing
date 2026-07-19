c24c89f29bf3c97ad262db4135cfa43a56c69562
Citrancs <h.marci1224@gmail.com>
2026-07-19 19:12:19 +0200

    login happy path ready,added search bar positive test at home page-guest

--- vinted-qa-automation/tests/test_home.py
+++ vinted-qa-automation/tests/test_home.py
@@ -1,5 +1,6 @@
 import pytest
 import allure
+import urllib.parse
 from pages.home_page import HomePage
 
 
@@ -14,10 +15,8 @@ from pages.home_page import HomePage
 def test_vinted_homepage_loads(browser, description):
     # 1. Példányosítjuk a HomePage-et, és átadjuk neki a 'browser' fixture-t
     home_page = HomePage(browser)
-
     # 2. Betöltjük az oldalt
     home_page.load()
-
     # 3. Ellenőrizzük (assert), hogy jó helyen járunk-e
     # Megnézzük, hogy a Vinted szó benne van-e az oldal címében (Title)
     assert "Vinted" in home_page.get_title(), "A Vinted főoldal nem töltődött be megfelelően!"
@@ -39,3 +38,25 @@ def test_navigate_to_login_page(browser, description):
 
     # ELLENŐRZÉS (Assert): Megnézzük, hogy az URL-ben szerepel-e a regisztrációs/bejelentkezési rész
     assert "signup" in home_page.get_current_url(), "Nem sikerült átnavigálni a bejelentkező oldalra!"
+
+
+@allure.title("TC03 - Search product without login")
+@allure.severity(allure.severity_level.CRITICAL)
+@allure.tag("Positive", "Smoke", "Navigation", "Homepage", "Guest")
+@pytest.mark.parametrize(
+    ("product_name", "description"),
+    [
+        ("póló", "Testing search product without login")
+    ])
+def test_searching_product(browser, product_name, description):
+    home_page = HomePage(browser)
+    home_page.load()
+    home_page.accept_cookies()
+    home_page.input_search_bar(product_name)
+    home_page.select_suggestion_by_index(0)
+    # 1. Lekérjük az aktuális URL-t a böngészőtől
+    actual_url = browser.current_url
+    # 2. Dekódoljuk az URL-t (hogy a %C3%B3-ból újra 'ó' legyen)
+    decoded_url = urllib.parse.unquote(actual_url)
+    # 3. Az ASSERT: megnézzük, hogy a "póló" kulcsszó benne van-e a tiszta URL-ben
+    assert "póló" in decoded_url, f"Hiba! A 'póló' szó nem található az URL-ben. Az aktuális URL: {decoded_url}"
--- vinted-qa-automation/tests/test_login.py
+++ vinted-qa-automation/tests/test_login.py
@@ -2,9 +2,6 @@ import pytest
 import allure
 from pages.login_page import LoginPage
 from pages.home_page import HomePage
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 @allure.title("TC01 - Login with e-mail and password")
@@ -15,18 +12,15 @@ from selenium.webdriver.support import expected_conditions as EC
     [
         ("test3@test.hu", "1234_Abcd", "Login with e-mail and password")
     ])
-def test_login_happy_path(browser,email, password, description):
+def test_login_happy_path(browser, email, password, description):
     login_page = LoginPage(browser)
     home_page = HomePage(browser)
     # Betölti a login oldalt
     login_page.load()
-
     home_page.accept_cookies()
-
-    # Megvárjuk, amíg a takaró süti banner teljesen ELTŰNIK a képernyőről!
-    WebDriverWait(browser, 5).until(
-        EC.invisibility_of_element_located((By.CLASS_NAME, "ot-dpd-container"))
-    )
     # Rákattintunk a "Bejelentkezés" gombra
     login_page.login_button_click()
     login_page.login_with_email_button_click()
+    login_page.input_username_or_email(email)
+    login_page.input_password(password)
+    login_page.button_submit_click()
