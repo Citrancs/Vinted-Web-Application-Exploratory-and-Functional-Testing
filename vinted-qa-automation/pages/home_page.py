195e683c8f4fc2389a0d52954240fcaf6be89722
Citrancs <h.marci1224@gmail.com>
2026-07-19 19:10:39 +0200

    home page and login page + locators

--- vinted-qa-automation/pages/home_page.py
+++ vinted-qa-automation/pages/home_page.py
@@ -15,15 +15,37 @@ class HomePage(GeneralPage):
         self.open_webpage(self.URL)
 
     def login_registration_button(self):
-        a_sign_in = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.XPATH, '//a[@data-testid="header--login-button"]')))
+        a_sign_in = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable(
+            (By.XPATH, '//a[@data-testid="header--login-button"]')))
         return a_sign_in
 
     def cookies_accept_button(self):
-        button_accept = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.ID,"onetrust-accept-btn-handler")))
+        button_accept = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable(
+            (By.ID,"onetrust-accept-btn-handler")))
         return button_accept
 
     def accept_cookies(self):
         self.cookies_accept_button().click()
+        # Megvárjuk, amíg a takaró süti banner teljesen ELTŰNIK a képernyőről!
+        WebDriverWait(self.browser, 5).until(
+            EC.invisibility_of_element_located((By.CLASS_NAME, "ot-dpd-container"))
+        )
 
     def click_login_button(self):
         self.login_registration_button().click()
+
+    def input_search_bar(self, product_name):
+        WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable(
+            (By.ID,"search_text"))).send_keys(product_name)
+
+    def select_suggestion_by_index(self, index=0):
+        # 1. Megvárjuk, amíg a javaslatok listája megjelenik a DOM-ban és láthatóvá válik
+        WebDriverWait(self.browser, 10).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, '[data-testid="item-suggestions"]'))
+        )
+
+        # 2. Összegyűjtjük az összes javaslat elemet egy listába
+        suggestions = self.browser.find_elements(By.CSS_SELECTOR, '[data-testid="search-suggestion-bg"]')
+
+        # 3. Rákattintunk a kért indexű elemre (alapértelmezetten a 0.-ra)
+        suggestions[index].click()
--- vinted-qa-automation/pages/login_page.py
+++ vinted-qa-automation/pages/login_page.py
@@ -1,7 +1,10 @@
+from selenium.webdriver.remote.webelement import WebElement
+
 from pages.general_page import GeneralPage
 from selenium.webdriver.common.by import By
 import selenium.webdriver.support.expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+
 
 class LoginPage(GeneralPage):
     URL = "https://www.vinted.hu/member/signup/select_type?ref_url=%2F"
@@ -16,8 +19,22 @@ class LoginPage(GeneralPage):
 
     def login_button_click(self):
         WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable(
-        (By.XPATH, '//span[@data-testid="auth-select-type--register-switch"]//span'))).click()
+            (By.XPATH, '//span[@data-testid="auth-select-type--register-switch"]//span'))).click()
 
     def login_with_email_button_click(self):
         WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable(
             (By.XPATH, '//span[@data-testid="auth-select-type--login-email"]//span'))).click()
+
+    def input_username_or_email(self, email):
+        email_field = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.ID, "username")))
+        email_field.clear()
+        email_field.send_keys(email)
+
+    def input_password(self, password):
+        password_field = WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.ID, "password")))
+        password_field.clear()
+        password_field.send_keys(password)
+
+    def button_submit_click(self):
+        WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable(
+            (By.XPATH, "//button[@type='submit']"))).click()
